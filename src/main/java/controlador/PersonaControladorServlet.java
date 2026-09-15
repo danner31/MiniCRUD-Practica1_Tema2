89958c658adb5c573c0764045e9e224adf5d6309
danner31 <dannercarneiro31@gmail.com>
2026-09-15 11:02:13 -0300

    Add email parameter to agregar method
    Added email parameter to agregar method and PersonaVO constructor.

--- src/main/java/controlador/PersonaControladorServlet.java
+++ src/main/java/controlador/PersonaControladorServlet.java
@@ -64,7 +64,8 @@ public class PersonaControladorServlet extends HttpServlet {
         int codigo = Integer.parseInt(request.getParameter("codigo"));
         String nombre = request.getParameter("nombre");
         double sueldo = Double.parseDouble(request.getParameter("sueldo"));
-        dao.agregar(new PersonaVO(codigo, nombre, sueldo));
+        String email = request.getParameter("email")
+        dao.agregar(new PersonaVO(codigo, nombre, sueldo, email));
         response.sendRedirect(request.getContextPath() + "/persona"); // PRG → listar
     }
 
